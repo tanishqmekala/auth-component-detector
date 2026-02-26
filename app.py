0cae644af9dce474bb09d48ca0267429f6db7454
tanishqmekala <tanishq.reddyy@gmail.com>
2026-02-25 18:34:10 -0600

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ DEFAULT_SITES = [
     'https://www.linkedin.com/login',
     'https://www.facebook.com/login',
     'https://login.salesforce.com/',
-    'https://login.twitter.com/i/flow/login',
+    'https://x.com/login',
 ]
 
 
@@ -427,7 +427,7 @@ footer a:hover{opacity:1;text-shadow:0 0 12px var(--red-glow);}
       <span class="chip" onclick="quick('https://www.linkedin.com/login')">LinkedIn</span>
       <span class="chip" onclick="quick('https://www.facebook.com/login')">Facebook</span>
       <span class="chip" onclick="quick('https://login.salesforce.com/')">Salesforce</span>
-      <span class="chip" onclick="quick('https://login.twitter.com/i/flow/login',)">Twitter</span>
+      <span class="chip" onclick="quick('https://x.com/login'/i/flow/login',)">X</span>
     </div>
   </div>
 
